27419767be217f0ad01cc9e8a63c56d47b391610
bigfanguy <30512818+bigfanguy@users.noreply.github.com>
2017-07-27 18:25:09 -0700

    added express
    test

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-// Load the http module to create an http server.
-var http = require('http');
+const express = require('express')
+const app = express()
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
