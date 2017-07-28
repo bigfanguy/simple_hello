7e2f840feeab42375f5bb64fd53236b76aba1e31
bigfanguy <30512818+bigfanguy@users.noreply.github.com>
2017-07-27 18:38:15 -0700

    fooling around
    yup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('<head>Doin Stuffs</head><body><div style="font-weight: bold;">Hello test</div></body>')
 })
 
 app.listen(3000, function () {
