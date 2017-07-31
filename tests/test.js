51151cd75e7844ca026b68526f3b71c5af21b7f1
bigfanguy <30512818+bigfanguy@users.noreply.github.com>
2017-07-30 20:39:06 -0700

    travis added, test folder added

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const server = require("./index.js");
+const server = require("../index.js");
 const should = chai.should();
 
 chai.use(chaiHttp);
