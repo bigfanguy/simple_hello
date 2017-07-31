91610d83333b68c17fb2a5694029c7c653e2e3bb
bigfanguy <30512818+bigfanguy@users.noreply.github.com>
2017-07-30 20:17:02 -0700

    We added the tasks endpoint and made some tests using mocha and chai.

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello test here')
-})
+const json = [
+  { id: 1, desc: "Cut hole in box." },
+  { id: 2, desc: "Put junk in box." },
+  { id: 3, desc: "Open the box." }
+];
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.get("/", function(req, res) {
+  res.send("Howdy!");
+});
+
+function getID(id, data) {
+  return data.filter(data => data.id === id);
+}
+
+function getDesc(desc, data) {
+  return data.filter(data => data.desc.includes(desc));
+}
+
+function getTasks(id, desc) {
+  if (typeof id === "undefined") {
+    id = "default";
+  }
+  if (typeof desc === "undefined") {
+    desc = "default";
+  }
+
+  if (id === "default" && desc === "default") {
+    return json;
+  } else if (id !== "default" && desc === "default") {
+    return getID(id, json);
+  } else if (id === "default" && desc !== "default") {
+    return getDesc(desc, json);
+  } else {
+    return getID(id, getDesc(desc, json));
+  }
+}
+
+// SHOW
+app.get('/tasks/:id', (req, res) => {
+  // ...
+});
+
+// INDEX
+app.get("/tasks", function(req, res) {
+  if (typeof req.query.desc === "undefined") {
+    req.query.desc = "default";
+  }
+  var json_send = getTasks(req.query.id, req.query.desc);
+  res.send(json_send);
+});
+
+var server = app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
+});
+
+module.exports = server;
---
+++ test.js
@@ -0,0 +1,51 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("./index.js");
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Tasks", function() {
+  it("should list ALL tasks on /tasks GET");
+  it("should list a list of tasks on /tasks?id=<id> GET");
+  it("should list a list of tasks on /tasks?desc=<desc> GET");
+  it("should list a list of tasks on /tasks?id=<id>desc=<desc> GET");
+});
+
+it("should list ALL tasks on /tasks GET", function(done) {
+  chai.request(server).get("/tasks").end(function(err, res) {
+    res.should.have.status(200);
+    res.should.be.json;
+    res.body.should.be.a("array");
+    done();
+  });
+});
+
+it("should list a list of tasks on /tasks?id=<id> GET", function(done) {
+  chai.request(server).get("/tasks?id=1").end(function(err, res) {
+    res.should.have.status(200);
+    res.should.be.json;
+    res.body.should.be.a("array");
+    done();
+  });
+});
+
+it("should list a list of tasks on /tasks?desc=<desc> GET", function(done) {
+  chai.request(server).get("/tasks?desc=box").end(function(err, res) {
+    res.should.have.status(200);
+    res.should.be.json;
+    res.body.should.be.a("array");
+    done();
+  });
+});
+
+it("should list a list of tasks on /tasks?id=<id>desc=<desc> GET", function(
+  done
+) {
+  chai.request(server).get("/tasks?id=1&desc=box").end(function(err, res) {
+    res.should.have.status(200);
+    res.should.be.json;
+    res.body.should.be.a("array");
+    done();
+  });
+});
